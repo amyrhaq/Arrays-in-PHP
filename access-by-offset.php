fcc5accb8068dcf4e8bf4f16ce240ef1d9a4e9c5
Your Name Here <amyr@amyrhaq.com>
2013-09-25 12:14:42 -0700

    completed lesson 4/7

---
+++ access-by-offset.php
@@ -0,0 +1,12 @@
+<!-- We've set up an array for you in the editor. Go ahead and use [] notation to echo or print the third element of the array. -->
+<html>
+<head>
+	<title>Access by offset with []</title>
+</head>
+<body>
+<?php 
+$tens = array(10, 20, 30, 40, 50);
+echo $tens[2];
+?>
+</body>
+</html>
