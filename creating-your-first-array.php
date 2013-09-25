7b214cbb177e65f3894ffe371d8908b89ebd7fa7
Your Name Here <amyr@amyrhaq.com>
2013-09-25 12:03:15 -0700

    completed lesson 3/7

---
+++ creating-your-first-array.php
@@ -0,0 +1,10 @@
+<!-- Create an array called $friends and put the names of three of your friends in it. Make sure each friend's name is in quotes!  -->
+<html>
+<head>
+	<title>Creating Your First Array</title>
+</head>
+<body>
+<?php
+$friends = array('Nick', 'Hakon', 'Chris'); ?>
+</body>
+</html>
