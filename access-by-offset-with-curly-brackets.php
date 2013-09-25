28c3429eae2a73a3eaf2d174a340f8c7b7b5b227
Your Name Here <amyr@amyrhaq.com>
2013-09-25 12:18:49 -0700

    completed lesson 5/7

---
+++ access-by-offset-with-curly-brackets.php
@@ -0,0 +1,12 @@
+<!-- Go ahead and update your previous code to use the {} syntax instead of []. -->
+<html>
+<head>
+	<title>Access by offset with {}</title>
+</head>
+<body>
+<?php  
+$tens = array(10, 20, 30, 40, 50);
+echo $tens{2};
+?>
+</body>
+</html>
