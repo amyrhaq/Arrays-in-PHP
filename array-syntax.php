6fe3231ddb4399aa55a00ef00b52e1e99a2b9480
Your Name Here <amyr@amyrhaq.com>
2013-09-25 12:00:27 -0700

    completed 2/7

---
+++ array-syntax.php
@@ -0,0 +1,11 @@
+<!-- Add two more items to our array, "Chips" and "Sausage". -->
+<html>
+<head>
+	<title>Array Syntax</title>
+</head>
+<body>
+<?php 
+$array = array('eggs', 'bread', 'milk', 'chips', 'sausage'); 
+?>
+</body>
+</html>
